674d6508ddf16fbb422643c0e9388c65da05b36c
giuliosmedile <giulio.smedile@hotmail.com>
2023-07-26 17:29:46 +0200

    anti alias

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set( 0, 5, 5 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio = window.devicePixelRatio;
 renderer.shadowMap.enabled = true;
